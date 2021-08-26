8e7a260ca4527740dacbac3443721e1469d56912
Thomas Witherow <twitherow@bellsouth.net>
2021-08-26 12:19:03 -0500

    Company exercise almost complete

--- exercises/Classes/Company.cs
+++ exercises/Classes/Company.cs
@@ -1,5 +1,8 @@
 
 using System;
+using System.Collections.Generic;
+using Classes;
+
 
 namespace Classes
 {
@@ -11,14 +14,24 @@ namespace Classes
         public DateTime CreatedOn { get; }
 
         // Create a public property for holding a list of current employees
-
+        public List<Employee> Employees { get; set; }
         /*
             Create a constructor method that accepts two arguments:
                 1. The name of the company
                 2. The date it was created
 
             The constructor will set the value of the public properties
-
         */
+        public Company(string name, DateTime StartDate, List<Employee> Employee)
+        {
+            Name = name;
+            CreatedOn = StartDate;
+            List<Employee> Employees = new List<Employee>() { };
+        }
+
+        public void ListEmployees(Employee Employee)
+        {
+            Console.WriteLine($"{Employee.FullName} works for {Name} as {Employee.Title} since {Employee.StartDate}");
+        }
     }
 }
